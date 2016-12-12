122e6c8094e874f4c36726e0cc23a54cabef860a
Nicolas Roybal <nicolas.roybal@hotmail.com>
2016-12-12 13:30:04 -0600

    Initial commit with all files

---
+++ main.js
@@ -0,0 +1,8 @@
+// Create DOM
+document.addEventListener("DOMContentLoaded", fuction(){
+  // Find elemements to modify later
+  // Crate instance of XHR object
+  // Open specific URL
+  // Listen for the "load event"
+  // Type casting 
+})
